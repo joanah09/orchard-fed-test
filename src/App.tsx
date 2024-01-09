5827b57d1e4a13cf45d1716061b9f5859d2cde39
DESKTOP-O4R0P87\Joanah <athrun.zala009@gmail.com>
2024-01-09 20:38:21 +0800

    removed scroll animation to make it simple

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,16 @@ import News from "./components/News";
 import { useSpring, animated } from "react-spring";
 
 function App() {
-  const [scrollOpacity, setScrollOpacity] = useState(0);
-
-  const newsComponent = useSpring({
-    opacity: scrollOpacity,
-    config: { duration: 400 },
-  });
-
-  const articlesComponent = useSpring({
+  const fadeAnimation = useSpring({
     opacity: 1,
     from: { opacity: 0 },
     config: { duration: 300 },
   });
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      const maxScroll = 700;
-      const opacity = Math.min(1, scrollPosition / maxScroll);
-      setScrollOpacity(opacity);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <div>
-      <animated.div style={articlesComponent}>
+      <animated.div style={fadeAnimation}>
         <Articles />
-      </animated.div>
-      <animated.div style={newsComponent}>
         <News />
       </animated.div>
     </div>
